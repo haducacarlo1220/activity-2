4dd62aaf95f70bf88b8a78af518aea8361fa1d4f
haducacarlo1220 <ralphhaduca@gmail.com>
2024-09-11 22:35:48 +0800

    Initial commit with variable.js

---
+++ activity2.js
@@ -0,0 +1,9 @@
+let name = "Ralph Carlo H.Pano";
+let whatDoYouWannaBecomeinYourLife = "programmer";
+let gender = "male";
+let twitterHandle = "@ralphcarlo";
+
+console.log('My name is ', name);
+console.log('I want to become a ', whatDoYouWannaBecomeinYourLife);
+console.log('My gender is ', gender);
+console.log('My twitter handle is ', twitterHandle);
